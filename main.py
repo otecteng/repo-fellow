4c49103f51d7711629856aa64eb4475d1641074e
Teng Li <otec.teng@gmail.com>
2020-02-10 00:03:04 +0800

    feature: command line ops support

--- main.py
+++ main.py
@@ -1,60 +1,59 @@
+"""repo crawler via command-line.
+
+Usage:
+    repo-fellow project <command> [<args>]
+    repo-fellow user <command> [<args>]
+    repo-fellow commit <command> [<args>]
+    repo-fellow event <command> [<args>]
+Options: 
+    -h,--help        
+
+Example:
+    repo-fellow projects remote owner
+    repo-fellow projects list
+    repo-fellow commit update *
+    repo-fellow commit update project_x
+"""
 import os
 import datetime
-import organization
-from parser import Parser
-from injector import Injector,Commit
+from docopt import docopt
 from crawler import Crawler
-from github_client import GithubClient
-from gitlab_client import GitlabClient
-# import gevent
+from injector import Injector,Commit
+from parser import Parser
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='Repo Fellow')
+    server, site, token = os.environ['GIT_SERVER'], os.environ['GIT_SITE'], os.environ['GIT_TOKEN']
+    db_user, db_password = "repo",os.environ['FELLOW_PASSWORD']
+    command = arguments["<command>"]
+    if arguments["project"]:
+        if command == "list":
+            print("not implemented")
+        if command == "remote":
+            commits = Crawler.create_client(server,site,token).getAllProjectCommitsCount(arguments["<args>"])
+            for i in commits:
+                if i["ref"]:
+                    print("{}:{}".format(i["name"],i["ref"]["target"]["history"]["totalCount"]))
+                else:
+                    print("[ERROR]: {} has none history record".format(i["name"]))
 
-def get_commits(project,since = None, limit = None):
-    commits = Crawler.create_client(os.environ['GIT_SERVER'],os.environ['GIT_SITE'],os.environ['GIT_TOKEN']).getProjectCommits(project, since = since, limit = limit )
-    commits = Parser.parse_commits(commits,format="github")
-    for j in commits:
-        j.project = project
-    # Injector(password = os.environ['FELLOW_PASSWORD']).insert_data(commits)
-    return commits
-
-def get_github_commit(commit):
-    try:
-        commit_info = client.getCommit(commit.project,commit.id)
-        commit = Commit.append_count_github(commit,commit_info)
-        injector.db_commit()
-    except Exception:
-        print("[error]:{}:{}".format(commit.project,commit.id))
-
-def get_github_projects():
-    projects = GithubClient(os.environ['GIT_SITE'],os.environ['GIT_TOKEN']).getProjects()
-    for i in Parser.parse_projects(projects,format = "github"):
-        print(i)
-    # Injector(password=os.environ['FELLOW_PASSWORD']).insert_data(Parser.parse_projects(projects))
-
-def get_gitlab_projects():
-    projects = GitlabClient(os.environ['GIT_SITE'],os.environ['GIT_TOKEN']).getProjects()
-    for i in Parser.parse_projects(projects,format = "gitlab"):
-        print(i)
-
-# read github project commits count by GraphQL API
-commits = Crawler.create_client(os.environ['GIT_SERVER'],os.environ['GIT_SITE'],os.environ['GIT_TOKEN']).getAllProjectCommitsCount("owner")
-for i in commits:
-    if i["ref"]:
-        print("{}:{}".format(i["name"],i["ref"]["target"]["history"]["totalCount"]))
-    else:
-        print("[ERROR]: {} has none history record".format(i["name"]))
-
-# injector = Injector(password = os.environ['FELLOW_PASSWORD'])
-# projects = injector.get_projcets()
-# for i in projects:
-#     print("update project commits:{}".format(i.path))
-#     commit = injector.get_project_last_commit(i.path)
-#     if commit is not None:
-#         new_commits = get_commits(commit.project,since = commit.created_at + datetime.timedelta(seconds=1))
-#     else:
-#         new_commits = get_commits(commit.project)
-#     print("new records of {} is:{}".format(i.path,len(new_commits)))
-#     Injector(password = os.environ['FELLOW_PASSWORD']).insert_data(new_commits)
-
-
-# g = [gevent.spawn(get, url) for url in urls]
-# gevent.joinall(g)
+    if arguments["commit"]:
+        if command == "update":
+            injector = Injector(password = db_password)
+            if arguments["<args>"]:
+                projects = [injector.get_project(arguments["<args>"])]
+            else:
+                projects = injector.get_projects()
+            for i in projects:
+                project = i.path
+                print("update project commits:{}".format(project))
+                crawler = Crawler.create_client(server,site,token)
+                commit = injector.get_project_last_commit(project)
+                if commit is not None:
+                    commits = crawler.getProjectCommits(project, since = commit.created_at + datetime.timedelta(seconds=1))
+                else:
+                    commits = crawler.getProjectCommits(project)
+                print("[INFO]:{} new commit number {}".format(project,len(commits)))
+                new_commits = Parser.parse_commits(commits,format="github")
+                for j in new_commits:
+                    j.project = project
+                Injector(password = db_password).insert_data(new_commits)
