33417d4932c024bb45fd4765f65de3a96c32cb23
Teng Li <otec.teng@gmail.com>
2020-02-11 23:27:22 +0800

    refactor:using logging to remove print

--- main.py
+++ main.py
@@ -46,46 +46,45 @@ if __name__ == '__main__':
         if command == "import":
             data = Crawler.create_client(server,site,token).getProjects()
             injector.insert_data(Parser.parse_projects(data))
-            print("[INFO] total imported projects {}".format(len(data)))
+            logging.info("total imported projects {}".format(len(data)))
         if command == "remote":
             commits = Crawler.create_client(server,site,token).getAllProjectCommitsCount(arguments["<args>"])
             for i in commits:
                 if i["ref"]:
-                    print("{}:{}".format(i["name"],i["ref"]["target"]["history"]["totalCount"]))
+                    logging.info("{}:{}".format(i["name"],i["ref"]["target"]["history"]["totalCount"]))
                 else:
-                    print("[ERROR]: {} has none history record".format(i["name"]))
+                    logging.error("{} has none history record".format(i["name"]))
 
     if arguments["user"]:
         if command == "import":
             data = Crawler.create_client(server,site,token).get_users()
             injector.insert_data(Parser.parse_users(data))
-            print("[INFO] total imported users {}".format(len(data)))
+            logging.info("total imported users {}".format(len(data)))
 
     if arguments["pr"]:
         if command == "import":
             project = arguments["<args>"]
             data = Crawler.create_client(server,site,token).get_pull_requests(project)
             injector.insert_data(Parser.parse_pulls(data))
-            print("[INFO] total imported pulls {}".format(len(data)))
+            logging.info("total imported pulls {}".format(len(data)))
 
     if arguments["commit"]:
         if command == "update":
             injector = Injector(db_user = db_user, db_password = db_password,database = db)
-            print("{}{}".format(db_user,db_password))
             if arguments["<args>"]:
                 projects = [injector.get_project(arguments["<args>"])]
             else:
                 projects = injector.get_projects()
             for i in projects:
                 project = i.path
-                print("update project commits:{}".format(project))
+                logging.info("update project commits:{}".format(project))
                 crawler = Crawler.create_client(server,site,token)
                 commit = injector.get_project_last_commit(project)
                 if commit is not None:
                     commits = crawler.getProjectCommits(project, since = commit.created_at + datetime.timedelta(seconds=1))
                 else:
                     commits = crawler.getProjectCommits(project)
-                print("[INFO]:{} new commit number {}".format(project,len(commits)))
+                logging.info("{} new commit number {}".format(project,len(commits)))
                 new_commits = Parser.parse_commits(commits,format="github")
                 for j in new_commits:
                     j.project = project
